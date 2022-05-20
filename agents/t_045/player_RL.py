dd3673bcde19fa273000d0df57245090cf27bd88
MinghuiSun <smh335789877@gmail.com>
2022-05-20 22:13:16 +0800

    add ring selection in mct RL

--- agents/t_045/player_RL.py
+++ agents/t_045/player_RL.py
@@ -1,5 +1,5 @@
-
 import sys
+
 sys.path.append('agents/t_045/')
 import json
 
@@ -11,6 +11,17 @@ import numpy as np
 
 TIMELIMIT = 0.95
 
+RING_BOARD = [[-1, -1, -1, -1, -1, -1, 4, 4, 4, 4, -1],
+              [-1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 4],
+              [-1, -1, -1, 4, 5, 6, 6, 6, 6, 5, 4],
+              [-1, -1, 4, 5, 6, 7, 7, 7, 6, 5, 4],
+              [-1, 4, 5, 6, 7, 8, 8, 7, 6, 5, 4],
+              [-1, 5, 6, 7, 8, 9, 8, 7, 6, 5, -1],
+              [4, 5, 6, 7, 8, 8, 7, 6, 5, 4, -1],
+              [4, 5, 6, 7, 7, 7, 6, 5, 4, -1, -1],
+              [4, 5, 6, 6, 6, 6, 5, 4, -1, -1, -1],
+              [4, 5, 5, 5, 5, 5, 4, -1, -1, -1, -1],
+              [-1, 4, 4, 4, 4, -1, -1, -1, -1, -1, -1]]
 
 
 class myAgent():
@@ -34,12 +45,30 @@ class myAgent():
         state = self.game_rule.generateSuccessor(state, action, self.id)
         return state.agents[self.id].score - score
 
+    def SelectRing(self, state):
+        current_max = -1
+        possible_list = []
+        ring_1, ring_2 = state.ring_pos
+        for pos in ring_1:
+            RING_BOARD[pos[0]][pos[1]] = -1
+        for pos in ring_2:
+            RING_BOARD[pos[0]][pos[1]] = -1
+        for i in range(10):
+            for j in range(10):
+                current_max = max(current_max, RING_BOARD[i][j])
+        for i in range(10):
+            for j in range(10):
+                if RING_BOARD[i][j] == current_max:
+                    possible_list += [(i, j)]
+        return random.choice(possible_list)
+
     def SelectAction(self, actions, currentState):
         self.round += 1
         start_time = time.time()
-        
+
         best_action = random.choice(actions)
         if self.round <= 5:
+            best_action['place pos'] = self.SelectRing(currentState)
             return best_action
 
         best_Q = -999
@@ -49,7 +78,7 @@ class myAgent():
                 print("time out!!!")
                 break
             Q_value = self.getQValue(deepcopy(currentState), action)
-            #print("get qValue:" + str(Q_value))
+            # print("get qValue:" + str(Q_value))
             if Q_value > best_Q:
                 best_Q = Q_value
                 best_action = action
@@ -57,47 +86,47 @@ class myAgent():
 
     def getQValue(self, state, action):
         features = self.getFeatures(state, action)
-        #print("get features: " + str(features))
+        # print("get features: " + str(features))
         if len(features) != len(self.weight):
             print("features and weight not matched!")
             return -999
         Q_value = 0
         for i in range(len(features)):
-            Q_value += features[i]*self.weight[i]
+            Q_value += features[i] * self.weight[i]
         return Q_value
 
     def getFeatures(self, state, action):
-        self_counter = 2*(self.id + 1)
-        opponent_counter = 2*(2-self.id)
+        self_counter = 2 * (self.id + 1)
+        opponent_counter = 2 * (2 - self.id)
         features = []
 
-        #feature1
+        # feature1
         next_state = deepcopy(state)
         score = self.DoAction(next_state, action)
         if score > 1:
             print("score is greater than 1: " + str(score))
         features.append(score)
 
-        #feature2
+        # feature2
         current_board = "".join(map(str, next_state.board))
-        self_counter_score = current_board.count(str(self_counter))/51
+        self_counter_score = current_board.count(str(self_counter)) / 51
         features.append(self_counter_score)
 
-        #feature3
-        oppo_counter_score = current_board.count(str(opponent_counter))/51
+        # feature3
+        oppo_counter_score = current_board.count(str(opponent_counter)) / 51
         features.append(oppo_counter_score)
 
-        #feature4
-        features.append(self.getStepScore(next_state.board)/21)
+        # feature4
+        features.append(self.getStepScore(next_state.board) / 21)
 
-        #feature5 棋盘中我方环周围的对方环数量
-        features.append(self.getComponentsAround(next_state, 2*(1-self.id)+1)/8)
+        # feature5 棋盘中我方环周围的对方环数量
+        features.append(self.getComponentsAround(next_state, 2 * (1 - self.id) + 1) / 8)
 
         return features
 
-#并没有严格【-1,1】
+    # 并没有严格【-1,1】
     def getStepScore(self, board):
-        self_ring = 2*(self.id+1)-1
+        self_ring = 2 * (self.id + 1) - 1
         max_value = 0
         hValue = 51
         for i in range(1, 10):
@@ -143,7 +172,7 @@ class myAgent():
                         value3 += (5 - slantValue) * 2
                     max_value = max(max_value, value3)
 
-        #print(max_value)
+        # print(max_value)
         return max_value
 
     def HeuristicValue(self, list, id):
@@ -172,7 +201,7 @@ class myAgent():
         if id == 1:
             hValue += max(simplify_list.count(2) - simplify_list.count(3), 0)
 
-        #print("get a h value: "+ str(hValue))
+        # print("get a h value: "+ str(hValue))
         return hValue
 
     def getComponentsAround(self, state, component):
@@ -180,17 +209,17 @@ class myAgent():
         self_rings = state.ring_pos[self.id]
         board = state.board
         larger_board = []
-        larger_board.insert(0, [5,5,5,5,5,5,5,5,5,5,5,5,5])
+        larger_board.insert(0, [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
         for i in range(11):
             bList = board[i].tolist()
-            bList.insert(0,5)
+            bList.insert(0, 5)
             bList.append(5)
             larger_board.append(bList)
-        larger_board.append([5,5,5,5,5,5,5,5,5,5,5,5,5])
+        larger_board.append([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
         new_board = np.array(larger_board)
         for self_ring in self_rings:
-            (i,j) = self_ring
-            print("ring: "+ str((i,j)))
+            (i, j) = self_ring
+            print("ring: " + str((i, j)))
             if new_board[i + 1][j] == component:
                 components += 1
             if new_board[i][j + 1] == component:
@@ -207,5 +236,5 @@ class myAgent():
                 components += 1
             if new_board[i - 1][j - 1] == component:
                 components += 1
-        print(components/len(self_rings))
-        return components/len(self_rings)
+        print(components / len(self_rings))
+        return components / len(self_rings)
